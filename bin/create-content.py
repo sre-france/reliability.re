6f7b646c73dc1aa5288f226a04b083f2b9bd8da5
Pablo SEMINARIO <pablo@seminar.io>
2020-09-20 16:48:49 +0200

    Reformatted bin/create-content.py using black

--- bin/create-content.py
+++ bin/create-content.py
@@ -16,10 +16,11 @@ def slugify(date, title):
     slug = title.lower()
     slug = slug.replace(" ", "-")
     date = date[:10]
-    return f'content/links/{date}-{slug}.md'
+    return f"content/links/{date}-{slug}.md"
+
 
 def main(filename):
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         data = json.load(f)
     title = data["issue"]["title"]
     created_at = data["issue"]["created_at"]
@@ -28,10 +29,12 @@ def main(filename):
     url = body.splitlines()[0].replace("url: ", "")
     description = "\n".join(body.splitlines()[2:])
     post_filename = slugify(created_at, title)
-    content = template.format(title=title, created_at=created_at, url=url, user=user, description=description)
+    content = template.format(
+        title=title, created_at=created_at, url=url, user=user, description=description
+    )
     print(post_filename)
     print(content)
-    with open(post_filename, 'w+') as f:
+    with open(post_filename, "w+") as f:
         print(content, file=f)
 
 

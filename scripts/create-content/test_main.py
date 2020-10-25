c5ffd8afb07071ddc8c57d10196d936355ae072c
Pablo SEMINARIO <pablo@seminar.io>
2020-10-25 22:37:33 +0100

    Add an example of the expected output for create-content script

--- scripts/create-content/test_main.py
+++ scripts/create-content/test_main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import unittest
+from unittest import mock
+from io import StringIO
+
+from main import main
 from main import slugify
 
 
@@ -31,6 +35,36 @@ class TestSlugyFunction(unittest.TestCase):
         expected = "content/links/2020-10-02-how-they-test.md"
         output = slugify(date, title)
         self.assertEqual(expected, output)
+
+
+class TestMain(unittest.TestCase):
+    @mock.patch("sys.stdout", new_callable=StringIO)
+    def test_parse_issue(self, mock_stdout):
+        json_data = r"""
+            {
+                "issue": {
+                    "title": "First article",
+                    "created_at": "2020-09-12T11:19:56Z",
+                    "user": {
+                        "login": "pabluk"
+                    },
+                    "body": "url: https://example.com\r\n---\r\nMarkdown content..."
+                }
+            }"""
+        expected = """content/links/2020-09-12-first-article.md
+---
+title: "First article"
+date: 2020-09-12T11:19:56Z
+github_username: pabluk
+twitter_username: pabluk
+link: https://example.com
+---
+Markdown content...
+
+"""
+        with mock.patch("main.open", mock.mock_open(read_data=json_data)):
+            main(None)
+            self.assertEqual(expected, mock_stdout.getvalue())
 
 
 if __name__ == "__main__":
